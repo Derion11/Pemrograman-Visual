fabfc6bfdac44be1020c80c48470b4caa896ed28
Derion11 <143385132+Tekku11@users.noreply.github.com>
2024-03-15 00:01:44 +0700

    Add files via upload

---
+++ PT1/plt1.py
@@ -0,0 +1,17 @@
+# Sumber kode sample dari matplotlib.org
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(19681)  # seed the random number generator.
+data = {'a': np.arange(50),
+        'c': np.random.randint(0, 50, 50),
+        'd': np.random.randn(50)}
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+
+fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+ax.scatter('a', 'b', c='c', s='d', data=data)
+ax.set_xlabel('x Axis label')
+ax.set_ylabel('y Axis label')
+
+plt.show()
